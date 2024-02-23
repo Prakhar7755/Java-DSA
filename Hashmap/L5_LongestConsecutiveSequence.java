4d375307ac6633798f13688b25844659ea3eddd0
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2024-02-23 09:00:19 +0530

    longest sequence

---
+++ Hashmap/L5_LongestConsecutiveSequence.java
@@ -0,0 +1,12 @@
+/*
+    I/P - 10  5   9   1   11  8   6   15  3   12  2
+    O/P - 8     9    10      11      12
+
+    steps -
+    1) make a map of int + boolean 
+    2) input all keys with true value
+    
+*/
+public class L5_LongestConsecutiveSequence {
+
+}
