b96ba1b4bffdbe7eb04b8174c19fc1f66048c0e3
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2024-03-26 17:42:36 +0530

    Tree day 1

---
+++ Trees/L1_Implementation.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class L1_Implementation {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Node root = createTree();
+        preorder(root);
+    }
+
+    static void preorder(Node root) {
+        if (root != null) {
+            System.out.printf("%d\t", root.data);
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    static Node createTree() {
+        Node root = null;
+        System.out.println("Enter data: ");
+        int data = sc.nextInt();
+
+        if (data == -1)
+            return null;
+
+        root = new Node(data);
+        System.out.println("Enter left for: " + data);
+
+        root.left = createTree();
+
+        System.out.println("Enter right for: " + data);
+        root.right = createTree();
+
+        return root;
+    }
+}
+
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
