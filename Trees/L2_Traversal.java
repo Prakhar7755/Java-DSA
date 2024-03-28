d38232f132724f18a4f254cb350501f2590b6f11
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2024-03-28 20:26:03 +0530

    Traversal

---
+++ Trees/L2_Traversal.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class L2_Traversal {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Node root = createTree();
+
+        preorder(root);
+        System.out.println();
+        inorder(root);
+        System.out.println();
+        postorder(root);
+        System.out.println();
+
+    }
+
+    static void preorder(Node root) {
+        if (root != null) {
+            System.out.printf("%d\t", root.data);
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    static void postorder(Node root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.printf("%d\t", root.data);
+        }
+    }
+
+    static void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.printf("%d\t", root.data);
+            inorder(root.right);
+        }
+    }
+
+    static Node createTree() {
+        Node root = null;
+        System.out.println("Enter data: ");
+        int data = sc.nextInt();
+
+        if (data == -1)
+            return null;
+
+        root = new Node(data);
+
+        System.out.println("Enter left for: " + data);
+        root.left = createTree();
+
+        System.out.println("Enter right for: " + data);
+        root.right = createTree();
+
+        return root;
+    }
+}
+
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
