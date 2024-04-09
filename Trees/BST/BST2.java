4f75fc426e5ba5dc456328e07a15252c6abbe02e
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2024-04-09 08:00:11 +0530

    deletion in BST

--- Trees/BST/BST2.java
+++ Trees/BST/BST2.java
@@ -1,61 +1,19 @@
-public class BinarySearchTree {
+public class BST2 {
 
     public Node root;
 
-    public BinarySearchTree() {
+    public BST2() {
         this.root = null;
     }
-
-    static Node deleteNode(Node node, int key) {
-        if (root == null)  return root;
-
-        if (key < root.data) 
-            root.left = deleteNode(root.left, key)
-        
-            else if (key>root.data) 
-            root.right = deleteNode(root.right, key);
-
-        else{ /*CASE THREE*/
-            if (root.left == null) 
-                return root.right; 
-            else if (root.right == null) 
-                return root.left;
-            
-            root.data = minValue(root.data);
-            root.right = deleteNode(root.right, root.data);
-        }
-        return root;
-    }
-
-    static int minValue(Node root) {
-        int minv = root.data;
-        while (root.left != null) {
-            minv = root.left.data;
-            root = root.left;
-        }
-        return minv;
-    }
-
-    static boolean search(Node root, int val) {/* SEARCHING IN BST */
-        if (root == null)
-            return false;
-        if (root.data == val)
-            return true;
-
-        if (root.data > val)
-            return search(root.left, val);
-
-        else
-            return search(root.right, val);
-    }
-
-    public void insert(int value) {/* inserting in bst */
+    
+    public void insert(int value) {
         if (root == null) {
             root = new Node(value);
         } else {
             insertRec(root, value);
         }
     }
+
 
     private void insertRec(Node current, int value) {
         if (value < current.data) {
@@ -82,7 +40,7 @@ public class BinarySearchTree {
     }
 
     public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();
+        BST2 bst = new BST2();
         bst.insert(50);
         bst.insert(30);
         bst.insert(70);
